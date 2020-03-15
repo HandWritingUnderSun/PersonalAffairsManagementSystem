a1b8fded57dd034e9704398e820412147f5dfa9b
Zhu Yu <zhuy1@126.com>
2020-03-15 08:03:48 +0800

    qweqwe

---
+++ PersonalAffairsManagementSystem/Models/User/UserLogonDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace PersonalAffairsManagementSystem.Models
+{
+    public class UserLogonDbContext:DbContext
+    {
+        //添加表S_Constant实体
+        public DbSet<UserLogon> s_Constants { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //配置Sqlserver连接字符串
+            //对于这个上下文重写此方法以配置要使用的数据库（和其他选项）。对于上下文的每个实例调用此方法创建。
+            //加载appsetting.json
+            IConfiguration configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json").Build();
+
+            string connectionString = configuration["ConnectionStrings:SqlServer"];
+            optionsBuilder.UseSqlServer(connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.con
+        }
+    }
+}
